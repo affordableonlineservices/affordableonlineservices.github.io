2bf65bd2ae88041f18a82ce11baee07d94ea1501
affordableonlineservices <affordableonlineservices@gmail.com>
2025-08-26 02:33:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+fuction sendMail(){
+  let parms = {
+    name : document.getElementById("name").value,
+    email : document.getElementById("email").value,
+    subject : document.getElementById("subject").value,
+    message : document.getElementById("message").value,
+  }
 
+  emailjs.send("service_dnnwurd","template_hmtuh4u",parms).then(alert("Email Sent!!"))
+}
